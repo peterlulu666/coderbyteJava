78e91ae708cd7c1b93d5d38f7c4d25296f1de030
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-22 17:23:48 -0500

    commit

---
+++ src/SimpleAdding/Main.java
@@ -0,0 +1,25 @@
+package SimpleAdding;
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static int SimpleAdding(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return num + SimpleAdding(num - 1);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.print(SimpleAdding(num));
+
+    }
+
+}
