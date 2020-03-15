22d39896abf5e477d83b5a706e9188197ffa837b
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-15 12:34:34 -0400

    commit

--- src/Easy10AalphabetSoup/AalphabetSoup.java
+++ src/Easy10AalphabetSoup/AalphabetSoup.java
@@ -19,6 +19,8 @@ Output: "ahhloop"
 package Easy10AalphabetSoup;
 
 
+import java.util.Arrays;
+
 public class AalphabetSoup {
     String alphabeticalOrder(String str) {
         // Declare characterArray
@@ -39,22 +41,66 @@ public class AalphabetSoup {
                 }
             }
         }
-        // Declare reverseStr
-        String reverseStr = "";
-        // Copy letter in characterArray to reverseStr
+        // Declare alphabeticalStr
+        String alphabeticalStr = "";
+        // Copy letter in characterArray to alphabeticalStr
         for (int i = 0; i < characterArray.length; i++) {
-            reverseStr = reverseStr + characterArray[i];
+            alphabeticalStr = alphabeticalStr + characterArray[i];
 
         }
 
-        // Return reverseStr
-        return reverseStr;
+        // Return alphabeticalStr
+        return alphabeticalStr;
+
+    }
+
+    String alphabeticalLetter(String str) {
+        // Copy letter to characterArray
+        char[] characterArray = str.toCharArray();
+        // Sort characterArray
+        Arrays.sort(characterArray);
+        // Return letter in alphabetical order
+        return new String(characterArray);
+
+    }
+
+    String alphabeticalStr(String str) {
+        // Sort string
+        String alphabeticalCharacter = "" + str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = 0; j < alphabeticalCharacter.length(); j++) {
+                if (str.charAt(i) < alphabeticalCharacter.charAt(j)) {
+                    // Add the letter to the left
+                    alphabeticalCharacter = str.charAt(i) + alphabeticalCharacter;
+                    break;
+
+                } else if (alphabeticalCharacter.length() - 1 == j) {
+                    alphabeticalCharacter = alphabeticalCharacter + str.charAt(i);
+                    break;
+
+                } else if (str.charAt(i) >= alphabeticalCharacter.charAt(j) &&
+                        str.charAt(i) <= alphabeticalCharacter.charAt(j + 1)) {
+                    alphabeticalCharacter =
+                            alphabeticalCharacter.substring(0, j + 1) +
+                                    str.charAt(i) +
+                                    alphabeticalCharacter.substring(j + 1);
+                    break;
+
+                }
+
+            }
+
+        }
+
+        return alphabeticalCharacter;
 
     }
 
     public static void main(String[] args) {
         var aalphabetSoup = new AalphabetSoup();
         System.out.println(aalphabetSoup.alphabeticalOrder("coderbyte"));
+        System.out.println(aalphabetSoup.alphabeticalLetter("coderbyte"));
+        System.out.println(aalphabetSoup.alphabeticalStr("coderbyte"));
 
     }
 
